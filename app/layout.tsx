3eb19c104045d24dd7b6bfe05d29db935012a8fa
jhyunwoo <jhyunwoo0228@gmail.com>
2023-06-24 22:05:37 +0900

    add analytics and ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,33 @@
-import Link from "next/link"
-import "./globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
-import { Analytics } from "@/components/analytics"
-import { ModeToggle } from "@/components/mode-toggle"
+import Link from "next/link";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
+import CookieBanner from "@/components/cookiebanner";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "jhyunwoo의 Blog",
   description: "jhyunwood의 개발 블로그",
-  other:{
-    "naver-site-verification":"b4c28f88711f54fdb70057b44ced1d05327bfe03"
-  }
-}
+  other: {
+    "naver-site-verification": "b4c28f88711f54fdb70057b44ced1d05327bfe03",
+  },
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="kr">
-    <body
+      <GoogleAnalytics GA_MEASUREMENT_ID="G-N8FZXTL1E5" />
+      <body
         className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
       >
+        <CookieBanner />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="max-w-2xl mx-auto py-10 px-4">
             <header>
@@ -38,9 +41,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
             </header>
             <main>{children}</main>
           </div>
-          <Analytics />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewirtes() {
+    return [
+      {
+        soruce: "/google-analytics",
+        destivation: "https://www.googletagmanager.com/gtag/js?id=G-N8FZXTL1E5",
+      },
+    ];
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
